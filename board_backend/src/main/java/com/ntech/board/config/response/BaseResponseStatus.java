220fb374f3b252db83a955d3606ed9432ac933a6
hyeong-jun-kim <gamebox27@naver.com>
2023-07-26 01:09:17 +0900

    feat : BaseEntity, BaseResponse 세팅 - #1

---
+++ board_backend/src/main/java/com/ntech/board/config/response/BaseResponseStatus.java
@@ -0,0 +1,39 @@
+package com.ntech.board.config.response;
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    /**
+     * 2000 : Request 오류
+     */
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+
+    /**
+     * 3000 : Response 오류
+     */
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    INTERNAL_SERVER_ERROR(false,4000,"서버 오류입니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message){
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
