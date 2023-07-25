cb1417eb6743968e924345e82978d2eaf0304e50
hyeong-jun-kim <gamebox27@naver.com>
2023-07-26 01:33:15 +0900

    feat : Error 핸들링용 Advice 로직 추가 - #1

---
+++ board_backend/src/main/java/com/ntech/board/config/response/BaseException.java
@@ -0,0 +1,12 @@
+package com.ntech.board.config.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends RuntimeException{
+    private BaseResponseStatus status;
+}
