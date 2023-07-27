9c57074551af55f2663c7ff4469111ad083a2619
hyeong-jun-kim <gamebox27@naver.com>
2023-07-28 01:17:28 +0900

    refactor : 게시글 페이징 수정, 하단 페이지 버튼기능 추가 - #9

--- board_frontend/src/services/postService.js
+++ board_frontend/src/services/postService.js
@@ -7,10 +7,21 @@ export default class postService {
     writePost(postWriteReq) {
         return $axiosInst
             .post('/post/create', postWriteReq)
-            .then( response => {
-                console.log(response)
-            }).catch( error => {
-                console.log(error);
+            // .then( response => {
+            //     console.log(response)
+            // }).catch( error => {
+            //     console.log(error);
+            // });
+    }
+    /**
+     * 게시글 목록 불러오기
+     */
+    getPostList(page){
+        return $axiosInst
+            .get('/post/list', {
+                params: {
+                    "page": page
+                }
             });
     }
 }
--- board_frontend/src/views/post/PostList.vue
+++ board_frontend/src/views/post/PostList.vue
@@ -3,9 +3,10 @@
     <div class="overflow-auto">
         <p class="mt-3">Current Page: {{ currentPage }}</p>
 
-        <b-table id="my-table" :items="items" :per-page="perPage" :current-page="currentPage" small></b-table>
-        <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" :aria-controls="my - table"
-            align="center"></b-pagination>
+        <b-table id="my-table" ref="table" :items="items" :pages="pageList" :current-page="currentPage"
+            medium></b-table>
+        <b-pagination-nav v-model="currentPage" :link-gen="linkGen" :total-rows="rows" :per-page="size"
+            align="center" :number-of-pages="pageList.length" first-number use-router @change="getPostList"></b-pagination-nav>
     </div>
 </template>
   
@@ -21,24 +22,14 @@ export default {
             end: 0,
             prev: false,
             next: false,
-
-            perPage: 3,
             currentPage: this.$route.query.page ? this.$route.query.page : 1,
-            items: [
-                { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
-                { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
-                { id: 3, first_name: 'Barney', last_name: 'Rubble' },
-                { id: 4, first_name: 'Betty', last_name: 'Rubble' },
-                { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
-                { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
-                { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
-                { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
-                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
-            ],
+            pageList: [],
+            items: []
         }
     },
-    mounted() {
-        this.getPostList()
+    created() {
+        console.log(this.currentPage)
+        this.getPostList(this.currentPage)
     },
     computed: {
         rows() {
@@ -46,13 +37,47 @@ export default {
         },
     },
     methods: {
-        async getPostList() {
-            this.postService.getPostList(this.currentPage)
-                .then(response => console.log(response.data.result))
+         async getPostList(page) { // 게시글 목록 받아오기
+             await this.postService.getPostList(page)
+                .then(response => {
+                    const result = response.data.result;
+                    
+                    // 게시글 내용 채우기
+                    for (var i = 0; i < result.contents.length; i++) {
+                        const content = result.contents[i]
+                        const item = new Object()
+                        item.제목 = content.title
+                        item.작성자 = content.writer
+                        item.작성일 = content.createdAt
+                        item.좋아요 = content.likeCnt
+                        item.조회수 = content.likeCnt
+
+                        this.items[i] = item
+                    }
+                    // 페이징에 필요한 값 받아오기
+                    this.totalPage = result.totalPage
+                    this.page = result.page
+                    this.start = result.start
+                    this.end = result.end
+                    this.size = result.size
+                    this.prev = result.prev
+                    this.next = result.next
+                    this.pageList = result.pageList
+
+                    this.$refs.table.refresh()
+                    console.log(this.items)
+                    console.log(result.contents)
+                })
                 .catch(error => {
                     console.log(error);
                 });
-        }
+        },
+        linkGen(pageNum) { // 버튼 클릭시 페이지 이동 함수
+            return {
+                path: '/posts',
+                query: { page: pageNum }
+            }
+        },
     }
 }
 </script>
