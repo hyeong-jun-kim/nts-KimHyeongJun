0b537b6b0f129bc2f0260877f59315bb4d12434a
hyeong-jun-kim <gamebox27@naver.com>
2023-07-30 09:23:35 +0900

    refactor: 페이징 테이블 필드 값 추가 - #29

--- board_frontend/src/views/post/PostList.vue
+++ board_frontend/src/views/post/PostList.vue
@@ -7,12 +7,13 @@
         </div>
 
         <!--게시글 목록 테이블-->
-        <b-table id="my-table" ref="table" :items="items" :pages="pageList" :current-page="currentPage" medium
-            style="cursor: pointer" @row-clicked="rowClickHandler"></b-table>
+        <b-table class="table-pagination" ref="table" :items="items" :fields="tableFields" :pages="pageList"
+            :current-page="currentPage" medium @row-clicked="rowClickHandler"></b-table>
 
         <!--페이지 이동 버튼-->
         <b-pagination-nav v-model="currentPage" :link-gen="linkGen" :total-rows="rows" :per-page="size" align="center"
-            :number-of-pages="pageList.length" first-number use-router @change="getPostPagingList"></b-pagination-nav>
+            :number-of-pages="totalPage" first-number use-router :show-ellipsis="false"
+            @change="getPostPagingList"></b-pagination-nav>
     </div>
 </template>
   
@@ -26,14 +27,19 @@ export default {
             totalPage: 0,
             page: 0,
             size: 0,
-            start: 0,
-            end: 0,
-            prev: false,
-            next: false,
             currentPage: this.$route.query.page ? this.$route.query.page : 1,
             pageList: [],
             postIdList: [],
             items: [],
+            tableFields: [
+                { key: 'postId', label: '번호' },
+                { key: 'writer', label: '작성자', },
+                { key: 'title', label: '제목' },
+                { key: 'createdAt', label: '작성일' },
+                { key: 'likeCnt', label: '좋아요' },
+                { key: 'viewCnt', label: '조회수' },
+                // ...
+            ]
         }
     },
     created() {
@@ -49,23 +55,25 @@ export default {
             await this.postService.getPostPagingList(page)
                 .then(response => {
                     const result = response.data.result;
+                    this.postIdList = []
+                    this.items = []
 
                     // 게시글 내용 채우기
                     for (var i = 0; i < result.contents.length; i++) {
                         const content = result.contents[i]
                         const item = new Object()
-                        item.번호 = content.postId
-                        item.작성자 = content.writer
+                        item.postId = content.postId
+                        item.writer = content.writer
 
                         // 새로 생성된 게시글이면 제목 앞에 New 추가
                         if (content.new)
-                            item.제목 = "[New] " + content.title
+                            item.title = "[New] " + content.title
                         else
-                            item.제목 = content.title
+                            item.title = content.title
 
-                        item.작성일 = content.createdAt
-                        item.좋아요 = content.likeCnt
-                        item.조회수 = content.viewCount
+                        item.createdAt = content.createdAt
+                        item.likeCnt = content.likeCnt
+                        item.viewCnt = content.viewCount
 
                         this.items[i] = item
                         this.postIdList[i] = content.postId
@@ -73,11 +81,7 @@ export default {
                     // 페이징에 필요한 값 받아오기
                     this.totalPage = result.totalPage
                     this.page = result.page
-                    this.start = result.start
-                    this.end = result.end
                     this.size = result.size
-                    this.prev = result.prev
-                    this.next = result.next
                     this.pageList = result.pageList
 
                     this.$refs.table.refresh() // 테이블 새로고침
@@ -92,14 +96,19 @@ export default {
                 query: { page: pageNum }
             }
         },
-        rowClickHandler(record) { // 테이블 행 클릭시 게시글 상세보기로 이동
+        rowClickHandler(item) { // 테이블 행 클릭시 게시글 상세보기로 이동
             router.push({
-                path: '/post/' + record.번호,
+                path: '/post/' + item.postId,
             });
         },
-        goToWritePage(){ // 글쓰기 페이지로 이동
+        goToWritePage() { // 글쓰기 페이지로 이동
             this.$router.push('/post/write')
         }
     }
 }
 </script>
+<style>
+.table-pagination {
+    cursor: pointer;
+}
+</style>
