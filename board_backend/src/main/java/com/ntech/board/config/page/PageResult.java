bde73356315b3c863e1e02308ff2bf7a5d51a071
hyeong-jun-kim <gamebox27@naver.com>
2023-07-30 09:08:23 +0900

    refactor: PageRequest 구조 변경 - #29

--- board_backend/src/main/java/com/ntech/board/config/page/PageResult.java
+++ board_backend/src/main/java/com/ntech/board/config/page/PageResult.java
@@ -2,11 +2,9 @@ package com.ntech.board.config.page;
 
 import lombok.Getter;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Getter
 public class PageResult<T> {
@@ -15,27 +13,12 @@ public class PageResult<T> {
     private int totalPage;
     private int page;
     private int size;
-    private int start, end;
-    private boolean prev, next;
-    private List<Integer> pageList;
-
     private List<T> contents;
 
     public PageResult(Page<T> page){
-        this.contents = page.stream().collect(Collectors.toList());
         this.totalPage = page.getTotalPages();
-
-        makePageList(page.getPageable());
-    }
-
-    private void makePageList(Pageable pageable){
-        this.page = pageable.getPageNumber() + 1;
-        this.size = pageable.getPageSize();
-
-        int tmpEnd = (int)(Math.ceil(page/10.0)) * 10;
-        this.start = tmpEnd - 9;
-        this.end = Math.min(this.totalPage, tmpEnd);
-        this.next = totalPage > tmpEnd;
-        this.pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        this.page = page.getPageable().getPageNumber() + 1;
+        this.size = page.getPageable().getPageSize();
+        this.contents = page.stream().collect(Collectors.toList());
     }
 }
